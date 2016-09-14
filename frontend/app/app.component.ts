4f05079aef111b57453b618fd30b56b81b0c32fc
cschieber <ch.schieber@gmail.com>
2016-09-14 13:11:15 +0200

    frontend setup

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,6 +1,41 @@
 import { Component } from '@angular/core';
+
+import { ExampleDirective } from './shared/index';
+
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  directives: [ ExampleDirective ] ,
+  template: `
+  	<h1>Eine App. Hallo {{company}}</h1>
+  	    <form *ngIf="isVisible">
+  	    <input 
+           type="text"
+           [(ngModel)]="name" name="test" >
+        <p example>Du bist der: {{name}}</p>
+    </form>
+
+	<button (click)="onSave()" >Save</button>
+	<button (click)="isVisible = !isVisible">Show/Hide</button>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent {
+
+	company = "Bytabo";
+	isVisible = true;
+	name;
+
+    onSave() {
+
+    	alert(this.name + " von " + this.company);
+
+    	var object = {
+    		name: this.name,
+    		company: this.company,
+    	};
+
+    }
+
+
+}
--- server.js
+++ server.js
@@ -8,8 +8,55 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
+ res.send('Hello world\n');
+});
+/*
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+*/
+
+
+/*'use strict';
+
+const express = require('express');
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+io.set("origins", "*:*");
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
   res.send('Hello world\n');
+
+
+
+	// Retrieve
+	var MongoClient = require('mongodb').MongoClient;
+
+	// Connect to the db
+	MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+	  if(!err) {
+	    console.log("We are connected");
+	  }
+	});
+
+
 });
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
+
+var currentPrice = 99;
+ 
+io.on('connection', function (socket) {
+	socket.emit('priceUpdate',currentPrice);
+	socket.on('bid', function (data) {
+		currentPrice = parseInt(data);
+		socket.emit('priceUpdate',currentPrice);
+		socket.broadcast.emit('priceUpdate',currentPrice);
+	});
+});*/
